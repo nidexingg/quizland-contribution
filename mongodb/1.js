ccf1c600f7dfcd35e6efe8560bd0553767662667
Chandara H. Wei <gdarademoire2003@gmail.com>
2023-11-05 14:35:45 +0700

    reinitialized

--- mongodb/1.js
+++ mongodb/1.js
@@ -1,1 +1,125 @@
+/* 
+QUIZLAND JSON DATA UPLOAD SOURCE CODE INSTRUCTION:
 
+1-WHEN YOU FULLY UPLOADED ALL 10 QUIZZES ALREADY, 
+YOU NEED TO CREATE A NEW JS FILE FOR NEW QUIZ TEST. YOU CANNOT ADD OTHER QUESTIONS MORE THAN 10.
+FOR RENAME EXAMPLE: vuequiz1.js, vuequiz2.js, vuequiz3.js,... AND SO ON.
+
+2-MAKE SURE THE ANSWER IN THE 'answer' BOX AND 'options' BOX ARE THE SAME, CANNOT HAVE ANY TYPOS
+SUCH AS SPACES, SYMBOLS OR TEXTCASE(UPPER/LOWERCASE) DIFFERENCES.
+
+WE ENCOURAGE YOU TO FILL THE 'option' FIRST, THEN YOU COPY AND PASTE THE RIGHT ANSWER INTO THE 'answer' BOX.
+*/
+
+let questions = [
+    {
+    numb: 1,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 2,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 3,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 4,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 5,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+        {
+    numb: 6,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 7,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 8,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 9,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+    {
+    numb: 10,
+    question: "xxx",
+    answer: "xxx",
+    options: [
+      "xxx",
+      "xxx",
+      "xxx",
+      "xxx"
+    ]
+  },
+];
